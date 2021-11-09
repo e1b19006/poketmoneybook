c404fecccf155f8bb287bf7d183ce6ecea588f9f
e1b19006 <e1b19006@oit.ac.jp>
2021-11-09 15:15:23 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/t5/poketmoneybook/PoketmoneybookApplication.java
@@ -0,0 +1,13 @@
+package oit.is.t5.poketmoneybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketmoneybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketmoneybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/t5/poketmoneybook/PoketmoneybookApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.t5.poketmoneybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketmoneybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
