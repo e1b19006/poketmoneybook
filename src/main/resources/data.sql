afe98922b62fe675cf1abdef501f4bd4fbfc0e0f
e1b19037 <e1b19037@oit.ac.jp>
2021-11-16 15:37:15 +0900

    データベースのスキューマ作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO kind(kind_name)VALUES('財布');
+INSERT INTO kind(kind_name)VALUES('銀行');
+INSERT INTO kind(kind_name)VALUES('クレジット');
+INSERT INTO type(type_name)VALUES('食料品');
+INSERT INTO type(type_name)VALUES('消耗品');
+INSERT INTO type(type_name)VALUES('パソコン');
+INSERT INTO type(type_name)VALUES('その他');
+
+INSERT INTO log(value, kind_id, type_id, date, time)VALUES(1500,1,1,'2021-11-30','12:30:10');
