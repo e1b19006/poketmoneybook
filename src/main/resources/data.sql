266aa27d2bad14a4a2a7ab960bf10495cbcbfa97
e1b19057 <e1b19057@oit.ac.jp>
2021-11-30 00:42:41 +0900

    表にデータの追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,7 @@ INSERT INTO type(type_name)VALUES('パソコン');
 INSERT INTO type(type_name)VALUES('その他');
 
 INSERT INTO log(value, kind_id, type_id, date, time)VALUES(1500,1,1,'2021-11-30','12:30:10');
+
+INSERT INTO log(value, kind_id, type_id, date, time)VALUES(-500,2,4,'2022-1-1','0:34:00');
+
+INSERT INTO log(value, kind_id, type_id, date, time)VALUES(100,3,2,'2022-2-1','13:22:05');
