84f326d5401f4e462255510b12c5bc44c6564f17
Suu121006 <e1b19088@oit.ac.jp>
2021-11-30 14:46:45 +0900

    logへのinsertの実装途中

--- src/main/java/oit/is/t5/poketmoneybook/model/RecordMapper.java
+++ src/main/java/oit/is/t5/poketmoneybook/model/RecordMapper.java
@@ -2,7 +2,9 @@ package oit.is.t5.poketmoneybook.model;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -12,5 +14,9 @@ public interface RecordMapper {
 
   @Select("select * from log where log_id = #{log_id}")
   Record selectById(int log_id);
+
+  @Insert("insert into log (value, kind_id, type_id, date, time) values(#{value}, #{kind_id}, #{type_id}, #{date}, #{time})")
+  @Options(useGeneratedKeys = true, keyColumn = "log_id", keyProperty = "log_id")
+  void insertLog(Record record);
 
 }
