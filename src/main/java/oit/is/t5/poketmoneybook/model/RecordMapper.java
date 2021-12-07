6089ba9677593b4263db5187fce6eb23ae0b7168
Suu121006 <e1b19088@oit.ac.jp>
2021-12-07 16:14:47 +0900

    db_user1

--- src/main/java/oit/is/t5/poketmoneybook/model/RecordMapper.java
+++ src/main/java/oit/is/t5/poketmoneybook/model/RecordMapper.java
@@ -9,14 +9,14 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface RecordMapper {
-  @Select("select * from log join kind on (log.kind_id = kind.kind_id) join type on (log.type_id = type.type_id) join user on (log.user_id = user.user_id)")
-  ArrayList<Record> selectAllRecord();
+  @Select("select * from log join kind on (log.kind_id = kind.kind_id) join type on (log.type_id = type.type_id) join user on (log.user_id = user.user_id) where log.user_id = #{user_id}")
+  ArrayList<Record> selectAllRecord(int user_id);
   // join user on (log.user_id = user.user_id)
 
   @Select("select * from log where log_id = #{log_id}")
   Record selectById(int log_id);
 
-  @Insert("insert into log ( value, kind_id, type_id, date, time) values(#{value}, #{kind_id}, #{type_id},CURRENT_DATE,CURRENT_TIME)")
+  @Insert("insert into log ( value, user_id, kind_id, type_id, date, time) values(#{value}, #{user_id}, #{kind_id}, #{type_id},CURRENT_DATE,CURRENT_TIME)")
   @Options(useGeneratedKeys = true, keyColumn = "log_id", keyProperty = "log_id")
   void insertLog(Record record);
 
